34b0802aa72df50467b12c7ce4087a8423613ed1
Rakesh Kumar <rakeshcusat@gmail.com>
2013-07-11 07:15:13 +0000

    minor modification

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 var filedata =""
 fs.readFile("index.html", function(err, data){
     if (err) throw err;
-    var buffer = new Buffer();
+    var buffer = new Buffer(100);
     buffer.write(data, "utf-8");
     filedata = buffer.toString("utf-8");
 });
