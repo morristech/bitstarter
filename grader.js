afc3faa8f62f08e9e75c30ac7427499d351b2801
Rakesh Kumar <rakeshcusat@gmail.com>
2013-07-12 05:47:13 +0000

    Added modification ot grader.js

--- grader.js
+++ grader.js
@@ -24,9 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-
+var URL_HTMLFILE_DEFAULT = "urlIndex.html";
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -35,17 +36,38 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+var assertUrlExists = function(fileUrl) { return fileUrl;};
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var urltoHtmlFile = function(fileUrl) {
+    var instr = fileUrl.toString();
+    var fileData = "";
+    var event = 0;
+    restler.get(instr).on('complete', function(result) {
+                        if (result instanceof Error) {
+                            console.log("Error : " + result.message);
+                            process.exit(1);
+                        } else {
+                            fileData = result;
+                            console.log("File Data : " + fileData);
+                        }
+                      });
+    wait_for_signal(event,5000); 
+    console.log(" after Signal : " + fileData);
+    return fileData;
+};
+
+
+var cheerioHtmlFile = function(fileData) {
+    return cheerio.load(fileData);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(fileData, checksfile) {
+    
+    $ = cheerioHtmlFile(fileData);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -64,11 +86,36 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), "")
+        .option('-u, --url <html_file_url>', 'url path to index.html', clone(assertUrlExists), "")
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var checkJson = ""
+     if (program.file) {
+        console.log("File option is selected");
+        checkJson = checkHtmlFile(fs.readFileSync(htmlfile), program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+
+    }else if (program.url) {
+//        console.log("URL option is selected");
+  //      console.log(" URL : " + program.url);
+        restler.get(program.url).on('complete', function(result) {
+                        if (result instanceof Error) {
+                            console.log("Error : " + result.message);
+                            process.exit(1);
+                        } else {
+                            checkJson = checkHtmlFile(result, program.checks);
+                            var outJson = JSON.stringify(checkJson, null, 4);
+                            console.log(outJson);
+
+                        }
+
+                      });
+ 
+    } else {
+        console.log("Please provide right options");
+     }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
